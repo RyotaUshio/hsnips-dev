a72c8a7e391f46ef3134feb31138583d3e5f4bd0
RyotaUshio <ushio@ms.k.u-tokyo.ac.jp>
2025-11-29 19:59:26 +0900

    fix math context detection

--- src/globals/latex.ts
+++ src/globals/latex.ts
@@ -3,9 +3,19 @@ export { autoFraction } from './lib/auto-fraction';
 export { inlineMathSuffix } from './lib/inline-math';
 import type { Context } from './lib/context';
 
-export function math(context: Context) {
-    return context.scopes.some(
-        scope =>
-            !scope.startsWith('comment.line') && scope.startsWith('meta.math'),
-    );
+export function math(context: Context): boolean {
+    let hasMetaMath = false;
+
+    for (const scope of context.scopes) {
+        if (
+            scope.startsWith('comment.line') ||
+            scope === 'punctuation.definition.string.begin.tex'
+        ) {
+            return false;
+        }
+
+        hasMetaMath ||= scope.startsWith('meta.math');
+    }
+
+    return hasMetaMath;
 }
--- src/globals/markdown.ts
+++ src/globals/markdown.ts
@@ -3,10 +3,16 @@ export { autoFraction } from './lib/auto-fraction';
 export { inlineMathSuffix } from './lib/inline-math';
 import type { Context } from './lib/context';
 
-export function math(context: Context) {
-    return context.scopes.some(
-        scope =>
-            !scope.startsWith('comment.line') &&
-            scope.startsWith('markup.math'),
-    );
+export function math(context: Context): boolean {
+    let hasMarkupMath = false;
+
+    for (const scope of context.scopes) {
+        if (scope.startsWith('comment.line')) {
+            return false;
+        }
+
+        hasMarkupMath ||= scope.startsWith('markup.math');
+    }
+
+    return hasMarkupMath;
 }
